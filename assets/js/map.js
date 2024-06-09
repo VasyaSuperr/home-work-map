db0c5116ece6d50b30b812937916f9e54f2578b5
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-06-09 12:14:57 +0300

    Additional task

--- assets/js/map.js
+++ assets/js/map.js
@@ -37,3 +37,15 @@ console.log(`Array of keys: `, keysNumeralsMap);
 
 const valuesNumeralsMap = [...numeralsMap.values()];
 console.log(`Array of values: `, valuesNumeralsMap);
+
+const textWithNumbers =
+  "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
+
+function numberToWord(someText) {
+  return someText
+    .split(" ")
+    .map((n) => (numeralsMap.has(+n) ? numeralsMap.get(+n) : n))
+    .join(" ");
+}
+
+console.log(numberToWord(textWithNumbers));
